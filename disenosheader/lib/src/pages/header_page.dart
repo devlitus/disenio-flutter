11c16b69b2721495391850f75cb1e98c9e337538
devlitus <developercarles@gmail.com>
2020-07-16 12:50:57 +0200

    Diseños de header

---
+++ disenosheader/lib/src/pages/header_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:disenosheader/src/widgets/headers.dart';
+
+class HeaderPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HeaderWave(),
+    );
+  }
+}
