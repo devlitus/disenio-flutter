cba6d41e80485318779e70e16790ed8c05536c67
devlitus <developercarles@gmail.com>
2020-07-17 09:45:35 +0200

    Diseños de headers

--- disenosheader/lib/src/widgets/headers.dart
+++ disenosheader/lib/src/widgets/headers.dart
@@ -215,3 +215,59 @@ class _HeaderWavePainter extends CustomPainter {
     return true;
   }
 }
+
+class HeaderWaveGradient extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderWaveGradientPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderWaveGradientPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Rect rect = Rect.fromCircle(
+      center: Offset(0.0, 55.0),
+      radius: 180,
+    );
+    final Gradient gradient = LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: <Color>[
+          Color(0xff6D05E8),
+          Color(0xffC012FF),
+          Color(0xff6D05FA),
+        ],
+        stops: [
+          0.2,
+          0.5,
+          1.0,
+        ]);
+    final paint = Paint()..shader = gradient.createShader(rect);
+    // Propiedades
+//    paint.style = PaintingStyle.stroke;
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 20.0;
+    final path = Path();
+    // Dibujar con el path y el paint
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.3,
+        size.width * 0.5, size.height * 0.25);
+    path.quadraticBezierTo(
+        size.width * 0.75, size.height * 0.20, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
